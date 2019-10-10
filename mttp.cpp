c73e7dac1cfe691a9bdce9386ede60366d61fa48
NatanGarcias <natans.garcias@gmail.com>
2019-10-09 22:58:42 -0300

    corringindo as instancias e fzd imprime

--- mttp.cpp
+++ mttp.cpp
@@ -2,15 +2,19 @@
 #include<omp.h>
 using namespace std;
 
+// Variaveis globais recebidas do arquivos de leitura 
+// São utilizadas em varias partes do codigo por isto estão aqui
+
 string instancia,tipo, tipoDistancia;
 	
 int dimensao, nItem, capacidade, nMochileiros;
 double vMin, vMax, aluguel;
-  
+
+// Guarda as informacoes referente a cada ladrão  
 struct Mochileiro{
-    vector<int> caminho;
-    vector< vector<int> > mochila; 
-    int pesoMochila;
+    vector<int> caminho; // Cidades em que o mochleiro passa 
+    vector< vector<int> > mochila; // Contem os itens de cada cidade que o mochileiro passou
+    int pesoMochila; // Peso da mochila com todos os itens que ele coletou
 
     // Construtor:
     Mochileiro(){
@@ -19,25 +23,88 @@ struct Mochileiro{
     }
 };
 
+// Guarda as informacoes referente a cada Item que pode ser roubado
 struct Item{
-    int index;
-    int lucro;
-    int peso;
-    int ondeTo; 
+    int index;  // Indice do item
+    int lucro;  // Valor daquele determinado item
+    int peso;   // Peso daquele item
+    int ondeTo; // Em qual cidade este item se encontra 
 };
 
+// Guarda as informacoes referente a cada Cidade incluindo os que estão nela
 struct Casa{
-    int idCasa;
-    long double x;
-    long double y;
-    vector<Item> itemCasa;
-    vector<bool> visited;
+    
+    int idCasa; // Indice da casa
+    long double x; // Posição do indice x no plano cartesiano 
+    long double y; // Posição do indice y no plano cartesiano
+    vector<Item> itemCasa; // Todos os itens que estão na casa 
+    vector<bool> visited; // Verifica se o item daquela cidade já foi roubado por algum ladrão
+    
 };
 
 int dist(Casa a, Casa b){
     return ceil( sqrt(((a.x-b.x)*(a.x-b.x)) + ((a.y-b.y)*(a.y-b.y))) );
 }
- 
+
+
+//Imprime instancia para o visualizador
+void imprimeInstancia( const vector<Mochileiro> &ladroes, const vector<Casa> &cidade){
+
+ofstream saida("saida.txt");
+
+for(int i=0; i<ladroes.size(); i++){
+    
+    saida<<"[";
+    for(int j=0; j<ladroes[i].caminho.size(); j++){
+        
+        //Imprime as cidades que o ladrao "i" passou
+        saida<< ladroes[i].caminho[j];
+        
+        if( j!= ladroes[i].caminho.size()-1 ){
+            saida<<",";
+        }
+        
+    }
+    saida<<"]\n";
+    
+    saida<<"["; 
+    
+    //Contando quantas virgulas teremos que imprimir
+    
+    int virgulas=0;
+
+    for(int j=0; j< ladroes[i].mochila.size(); j++){
+        if(ladroes[i].mochila[j].size()!=0){
+            virgulas++;
+        }
+    }
+    virgulas-=1;
+    
+    for(int j=0; j< ladroes[i].mochila.size(); j++){
+        
+        if(ladroes[i].mochila[j].size()==0){ // Sem itens roubados pelo ladrao i na cidade j
+            continue;
+        }
+        
+        for(int k=0; k< ladroes[i].mochila[j].size(); k++){
+            saida<< ladroes[i].mochila[j][k];
+            
+            if(k!=ladroes[i].mochila[j].size()-1)
+                saida<<",";
+        }
+        
+        if( virgulas>0 ){ //Se ainda posso imprimir uma virgula
+            virgulas--;
+            saida<<",";
+        }
+            
+    }
+    
+    saida<<"]\n";
+}
+saida.close();
+}
+
 void imprimir( vector<Mochileiro>&ladroes, string &instancia){
 
     cout << "Instancia : " << instancia << endl; 
@@ -46,19 +113,19 @@ void imprimir( vector<Mochileiro>&ladroes, string &instancia){
         cout << "O Mochileiro " << i+1 << " pegou " << ladroes[i].caminho.size()-1  << " itens e passou nas cidades: \n";
 
         for(int j=0; j<ladroes[i].caminho.size()-1 ;j++){
-            cout << '[' << ladroes[i].caminho[j] << "] ";
+            cout << '[' << ladroes[i].caminho[j]+1 << "] ";
         }
         
         cout << '[' << ladroes[i].caminho[ ladroes[i].caminho.size()-1 ] << "] ";
         
-        cout << " E pegou os itens:\n";
-        
+        cout << "\nE pegou os itens:\n";
+
         for(int j=0;j<ladroes[i].caminho.size();j++){
             
             int qualCasa = ladroes[i].caminho[j];
 
             for(int k=0; k< ladroes[i].mochila[qualCasa].size();k++)
-                cout << '[' << ladroes[i].mochila[qualCasa][k] << "] ";
+                cout << '[' << ladroes[i].mochila[qualCasa][k]+1 << "] ";
             
         }
         
@@ -152,56 +219,63 @@ void calculaDistCasas(const vector<Casa> &cidade, vector<vector<int> > &distCasa
 }
 
 double fObj(const vector<Mochileiro> &ladroes, const vector<Item> &itens, const vector<Casa> &cidade, 
-             const vector<vector<int> > &distCasas, int W){ 
+             const vector<vector<int> > &distCasas, int W, int i){ 
     /*Funcao objetivo considera que o caminho de cada mochileiro possui IMPLICITAMENTE a saida da Origem
     e a chegada na Origem. Ex: Um caminho 0->1->2->0 sera representado no vector caminho por {1,2} */
     double resultado=0;
+    int pesoDaMochila=0;
+        
+    //Calculo da primeira parte da funcao objetivo para o ladrao em questao: Lucro!
     
-    for(int i=0;i<ladroes.size();i++){ // Para cada mochileiro...
-        int pesoDaMochila=0;
-        
-        //Calculo da primeira parte da funcao objetivo para o ladrao em questao: Lucro!
-        
-        double somatorioParte2=0;
+    double somatorioParte2=0;
 
-        //Da origem ate o primeiro...
+    //Da origem ate o primeiro...
+    double parteSuperiorFracaoInicial = distCasas[0][ladroes[i].caminho[0]];
+    double parteInferiorFracaoInicial = vMax- (( ( vMax-vMin ) / W ) * pesoDaMochila );
+    
+    somatorioParte2+= parteSuperiorFracaoInicial/parteInferiorFracaoInicial; //Da ultima casa ate a origem
 
-        double parteSuperiorFracaoInicial = distCasas[0][ladroes[i].caminho[0]];
-        double parteInferiorFracaoInicial = vMax- (( ( vMax-vMin ) / W ) * pesoDaMochila );
-        somatorioParte2+= parteSuperiorFracaoInicial/parteInferiorFracaoInicial; //Da ultima casa ate a origem
-        
+    int tamanhoCaminhoLadrao= ladroes[i].caminho.size();
 
-        int tamanhoCaminhoLadrao= ladroes[i].caminho.size();
+    for(int j=0;j<tamanhoCaminhoLadrao;j++){
 
-        for(int j=0;j<tamanhoCaminhoLadrao;j++){
-
-            if(j<tamanhoCaminhoLadrao-1){
-                double parteSuperiorFracao = distCasas[ladroes[i].caminho[j]][ladroes[i].caminho[j+1]];
-                double parteInferiorFracao= vMax- ( ( ( vMax-vMin ) / W*(1.0) ) * pesoDaMochila );
-                somatorioParte2+= parteSuperiorFracao/parteInferiorFracao ;
-            }
-            
-            for(int k=0;k<ladroes[i].mochila[j].size();k++){
-                
-                int qualItem = ladroes[i].mochila[j][k];
-                
-                resultado+= itens[qualItem].lucro;
-                pesoDaMochila+= itens[qualItem].peso;
-                
-            }
-
+        if(j<tamanhoCaminhoLadrao-1){
+           
+            double parteSuperiorFracao = distCasas[ladroes[i].caminho[j]][ladroes[i].caminho[j+1]];
+            double parteInferiorFracao= vMax- ( ( ( vMax-vMin ) / W*(1.0) ) * pesoDaMochila );
+           
+            somatorioParte2+= parteSuperiorFracao/parteInferiorFracao ;
         }
-        //Do ultimo ate a origem...
-        double parteSuperiorFracaoFinal= distCasas[ladroes[i].caminho[tamanhoCaminhoLadrao-1]][0];
-
-        double parteInferiorFracaoFinal= vMax-( ((vMax-vMin )/ W) * pesoDaMochila) ;
-        somatorioParte2+= parteSuperiorFracaoFinal/parteInferiorFracaoFinal; //Da ultima casa ate a origem
         
-        resultado+= (-1)*aluguel*somatorioParte2;
+        for(int k=0;k<ladroes[i].mochila[j].size();k++){
+            
+            int qualItem = ladroes[i].mochila[j][k];
+            
+            resultado+= itens[qualItem].lucro;
+            pesoDaMochila+= itens[qualItem].peso;
+        }
+
     }
+    //Do ultimo ate a origem...
+    double parteSuperiorFracaoFinal= distCasas[ladroes[i].caminho[tamanhoCaminhoLadrao-1]][0];
+
+    double parteInferiorFracaoFinal= vMax-( ((vMax-vMin )/ W) * pesoDaMochila) ;
+    somatorioParte2+= parteSuperiorFracaoFinal/parteInferiorFracaoFinal; //Da ultima casa ate a origem
+    
+    resultado-= aluguel*somatorioParte2;
     return resultado;
 }
 
+double fObj(const vector<Mochileiro> &ladroes, const vector<Item> &itens, const vector<Casa> &cidade, 
+             const vector<vector<int> > &distCasas, int W){ 
+    
+    double resultado=0;
+    
+    for(int i=0;i<ladroes.size();i++) // Para cada mochileiro...
+        resultado+= fObj(ladroes,itens,cidade,distCasas,W,i);
+    
+    return resultado; 
+}
 
 void roubo(int qualMochileiro, vector<Casa> &cidade, vector<Item> &itens, vector<Mochileiro> &ladroes, 
 				int cap, const vector<pair<double,pair<int,int > > > &custoBeneficio, vector<vector<int>> &distCasas){
@@ -398,5 +472,10 @@ int main( int argc, char** argv ){
   	cout<<"Resultado do greedy: "<<greedyResult<<endl;
 
   	imprimir(ladroes, instancia);
+
+    cout<<"\n\nImprimindo a instancia: \n";
+    imprimeInstancia(ladroes,cidade);
+    
   	
-}
+}   
+
