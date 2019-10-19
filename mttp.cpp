80312a928a9962cc16b43d206db916b09b8f19a3
NatanGarcias <natans.garcias@gmail.com>
2019-10-19 11:27:52 -0300

    pd da mochila

--- mttp.cpp
+++ mttp.cpp
@@ -430,14 +430,56 @@ double greedyThree(vector<Casa> &cidade, vector<Item> &itens, vector<Mochileiro>
 double greedy(vector<Casa> &cidade, vector<Item> &itens, vector<Mochileiro> &ladroes, string &tipo, 
 	vector<vector<int>> &distCasas){
     
-    if(tipo == " bounded strongly corr"){
+    if(tipo == " bounded strongly corr"){ //os valores dos itens estão fortemente relacionados a seus pesos.
         return greedyOne(cidade, itens, ladroes, distCasas);
     }
-    else if(tipo == " uncorrelated"){ //Depois colocar Two
+    else if(tipo == " uncorrelated"){ //os valores dos itens não estão relacionados a seus pesos. //Depois colocar Two
     	return greedyOne(cidade, itens, ladroes, distCasas);
     }
-    else if(tipo == " uncorrelated, similar weights"){ // Depois colocar Three
-    	return greedyOne(cidade, itens, ladroes, distCasas);
+    else if(tipo == " uncorrelated, similar weights"){ // os valores dos itens não estão relacionados a seus pesos, mas os
+    	return greedyOne(cidade, itens, ladroes, distCasas); // pesos de todos os itens são similares // Depois colocar Three
+    }
+}
+
+void completa(vector<Item> &itens, vector<int> &val, vector<int> &wt){
+
+    for(int j=0;j<itens.size();j++){
+        val.push_back(itens[j].lucro);
+        wt.push_back(itens[j].peso);
+    }
+}
+
+void pdDaMochila(vector<int> val, vector<int> wt, int n, int W){
+    
+    // array to store final result 
+    //dp[i] stores the profit with KnapSack capacity "i" 
+    int dp[W+1]; 
+      
+    //initially profit with 0 to W KnapSack capacity is 0 
+    memset(dp, 0, sizeof(dp)); 
+  
+    // iterate through all items 
+    for(int i=0; i < n; i++)  
+        //traverse dp array from right to left 
+        for(int j=W; j>=wt[i]; j--) 
+            dp[j] = max(dp[j] , val[i] + dp[j-wt[i]]); 
+    /*above line finds out maximum of  dp[j](excluding ith element value) 
+      and val[i] + dp[j-wt[i]] (including ith element value and the 
+      profit with "KnapSack capacity - ith element weight") */    
+    
+    for(int i=0;i<W+1;i++){
+        cout << dp[i] << " "; 
+    }
+    cout << endl;
+}
+
+void mochila(vector<Casa> &cidade,vector<Item> &itens,int capacidadeDaMochila, int qualTipo){
+
+    if(qualTipo == 1){
+        vector<int> val;
+        vector<int> wt;
+        completa(itens,val,wt);
+        pdDaMochila(val,wt,itens.size(),capacidadeDaMochila);
     }
 }
 
@@ -491,6 +533,8 @@ int main( int argc, char** argv ){
   	vector<vector<int> > distCasas(dimensao,vector<int>(dimensao,0));
   	
   	calculaDistCasas(cidade,distCasas);
+    
+    //mochila(cidade,itens,capacidade,1);
 
   	ofstream saida("saida.txt");
 
